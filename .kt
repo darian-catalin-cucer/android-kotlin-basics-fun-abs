44be236d3b181177ad87f16ef20fb413cd98b04d
Darian-Cătălin Cucer <86965240+cucerdariancatalin@users.noreply.github.com>
2023-02-17 16:14:04 +0200

    Create .kt

---
+++ .kt
@@ -0,0 +1,15 @@
+fun main() {
+    val x = -10
+    val y = 20
+    val z = -15.5
+
+    println("Absolute value of $x is ${x.absoluteValue}")
+    println("Absolute value of $y is ${y.absoluteValue}")
+    println("Absolute value of $z is ${z.absoluteValue}")
+}
+
+//Output:
+
+Absolute value of -10 is 10
+Absolute value of 20 is 20
+Absolute value of -15.5 is 15.5
